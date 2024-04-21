3da6847effff3658749deaf8cc6a543150628132
Cami Ikehara <camiikehara@gmail.com>
2024-04-20 21:41:53 -0300

    desafio 2

---
+++ Desafio2/js/index.js
@@ -0,0 +1,7 @@
+let producto1 = "lapicera"
+let precioU = 1000
+let cantidad
+alert ("Hola, estas por comprar un producto que es: "+ producto1 +" y su precio es de: "+ precioU)
+let cantidadDeseada = prompt ("Ingrese la cantidad que quiere comprar: ")
+const costoTotal = precioU * cantidadDeseada
+alert("Usted compró "+ cantidadDeseada +" "+producto1+" "+"el costo total es: $" + costoTotal)
